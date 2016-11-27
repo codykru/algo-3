f4f9157f1348753a2b61cad9afe7cde67bbb5ee4
codykru <codykru@gmail.com>
2016-11-26 22:26:08 -0500

    got the logic kind of worked out, need to implement queue, and use an array of pointers

--- test.cpp
+++ test.cpp
@@ -2,9 +2,57 @@
 #include <iomanip>
 #include <vector>
 #include <fstream>
+#include <string>
+#include <queue>       
+
 
 using namespace std;
 
+
+struct vertex {
+	int value; 		//the binary we are dealing with 
+	bool discovered;
+	int col;
+	int row; 
+	int level;
+}; 
+
+void findNeighbors(string value, int i, int j, int k){
+	cout << "we are at: " << i << ", " << j << ", " << k <<  endl;
+	//floor = i 
+	//row = j 
+	//col = k
+	//N E S  W U D 
+	if(value[0] ==  '1'){
+		cout << "NORTH!   " ;
+		cout << "go to: " << i << ", " << j - 1 << ", " << k <<  endl;
+		//findNeighbors(
+	}
+	if(value[1] ==  '1'){
+		cout << "EAST!    " ;
+		cout << "go to: " << i << ", " << j << ", " << k + 1 <<  endl;
+	}
+	if(value[2] ==  '1'){
+		cout << "SOUTH!   " ;
+		cout << "go to: " << i << ", " << j + 1<< ", " << k <<  endl;
+	}
+	if(value[3] ==  '1'){
+		cout << "WEST     " ;
+		cout << "go to: " << i << ", " << j << ", " << k - 1 <<  endl;
+	}
+	if(value[4] ==  '1'){
+		cout << "UP!      " ;
+		cout << "go to: " << i + 1<< ", " << j << ", " << k  <<  endl;
+	}
+	if(value[5] ==  '1'){
+		cout << "DOWN!    " ;
+		cout << "go to: " << i -1 << ", " << j << ", " << k <<  endl;	
+	}
+	cout << endl;
+
+}
+
+	
 int main(int argc, char* argv[]) {
 	int numMazes;
 	int levels, rows, columns;
@@ -17,15 +65,19 @@ int main(int argc, char* argv[]) {
 	int n = 0;					//length of our case
 	int value = 0;
 	int sumofNum =0;
+	queue<string> working;
 	ifstream inputFile;
 
 	if(argc != 2) {
-			cout << "\nInvalid number of arguments. Usage: ./project3 input.txt\n" << endl;
+			cout << "\nInvalid number arguments. \
+						Usage: ./project3 input.txt\n" << endl;
 			return -1;
 	}
+
 	inputFile.open(argv[1]);
 	if(!inputFile.is_open()) {
-		cout << "\nFile \'" << argv[1] << "\' could not be opened. Check the file and try again.\n" << endl;
+		cout << "\nFile \'" << argv[1] << "\' could not be opened. \
+							Check the file and try again.\n" << endl;
 		return -1;
 	}
 
@@ -47,17 +99,24 @@ int main(int argc, char* argv[]) {
 		cout << "start: (" << startL << ", " << startR << ", " << startC << ")" <<  endl;
 		cout << "end:   (" << endL << ", "  << endR << ", " << endC <<")"<< endl;
 
-		int map[levels][rows][columns] = {}; 
+		string map[levels][rows][columns] = {}; 
 		for( int i = 0 ; i < levels; i++){
 			cout << endl << endl << "level " << i <<": ";
 			for(int j = 0; j < rows; j++){
-				cout << endl << "row " << j << ": ";
+				cout << endl << "row " << j << ": " << endl;
 				for(int k = 0; k < columns; k++){ 
 					inputFile >> map[i][j][k];
-					cout  << setw(6) << map[i][j][k] << " " ;
+					//findNeighbors(map[i][j][k], i, j, k);
+					//cout  << setw(6) << map[i][j][k] << " " ;
+					
 				}
 			}
-		}		
+		}
+
+		//While(we aren't at end position && Q isn't empty) 
+			findNeighbors(map[startL][startR][startC], startL, startR, startC);
+		
+		
 	}
 }
 
